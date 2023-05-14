859d3089beddad47f350d98bbedce042ba3a895c
Avez-Anwar <avezanwar6@gmail.com>
2023-05-14 13:45:08 +0530

    New April Update for useEffect hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [post, setPost] = useState('posts');
+  // const [user, setUset] = useState('user');
+  // const [comment, setComment] = useState('comment');
+  const[items,setItems] = useState([])
+
+  // const handlePostClick=()=>{
+  //   setPost('posts')
+  // }
+  // const handleUserClick=()=>{
+  //   setPost('user')
+  // }
+  // const handleCommentClick=()=>{
+  //   setPost('comment')
+  // }
+
+  useEffect(()=>{
+    fetch(`https://jsonplaceholder.typicode.com/${post}`)
+      .then(response => response.json())
+      .then(json => setItems(json))
+    console.log("post useEffect");
+    
+  },[post])
+
+  return <div className="App">
+    <button onClick={()=>setPost('post')}>post</button>
+    <button onClick={()=>setPost('user')}>user</button>
+    <button onClick={()=>setPost('comment')}>comment</button>
+    <h3>{post}</h3>
+    {items.map((item)=>{
+     return <pre>{JSON.stringify(item)}</pre>
+    })}
+  </div>;
 }
 
 export default App;
